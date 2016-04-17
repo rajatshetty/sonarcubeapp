450e657f2ede3945aed22caf59e3b85d6dfb857f
Rajat Shetty <rajat_shetty@live.com>
2016-04-17 20:55:03 +0530

    Checking in

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myvariable = 0;
+
+            Sonar sonar = new Sonar();
+
+            sonar.Welcom();
+
+            CSharp csharp = new CSharp();
+
+            csharp.SetDetails("4.0", "Asp.Net MVC");
+
+            csharp.GetDetails();
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Sonar
+
+    {
+        public void Welcom()
+        {
+            Console.WriteLine("Welcom to Sonar Qube"); Console.WriteLine("We will analyze .NET projects here...");
+        }
+    }
+
+    public class CSharp
+    {
+        string Version;
+
+        string Technology;
+
+        public void GetDetails()
+
+        {
+
+            Console.WriteLine("version : " + Version);
+
+            Console.WriteLine("Technology : " + Technology);
+
+        }
+
+        public void SetDetails(string version, string technology)
+        {
+
+            Version = version; Technology = technology;
+
+        }
+
+    }
+}
