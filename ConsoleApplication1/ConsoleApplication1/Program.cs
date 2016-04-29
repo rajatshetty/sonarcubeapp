0b76f2a8689c26eea7feee16a241ab2352001a51
Rajat Shetty <rajat_shetty@live.com>
2016-04-29 07:39:07 +0530

    Commit!

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             int myvariable = 0;
-
+            //this is bug free
             int kvariable = 100;
 
             int xyzvariable = 200;
